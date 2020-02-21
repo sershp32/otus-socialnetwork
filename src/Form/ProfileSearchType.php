46c11126db9c6fc080c9072dd09d324ca43c1efe
Sergey Shpak <sergey.shpak@synesis.ru>
2020-02-21 13:14:48 +0300

    disabled csrf

--- src/Form/ProfileSearchType.php
+++ src/Form/ProfileSearchType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 final class ProfileSearchType extends AbstractType
 {
@@ -18,5 +19,12 @@ final class ProfileSearchType extends AbstractType
             ->add('lastName', TextType::class)
             ->add('search', SubmitType::class)
         ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'csrf_protection' => false,
+        ]);
     }
 }
